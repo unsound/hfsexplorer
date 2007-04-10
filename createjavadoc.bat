78b49325ceadaa276f9c68ece642bca5057cad92
Erik Larsson <catacombae@gmail.com>
2007-04-10 17:05:28 +0000

    Added planning document for write support and updated javadoc scripts.

--- createjavadoc.bat
+++ createjavadoc.bat
@@ -2,7 +2,7 @@
 setlocal
 pushd "%~dp0"
 
-set SOURCEFILES=..\src\org\catacombae\hfsexplorer\*.java ..\src\org\catacombae\hfsexplorer\apm\*.java ..\src\org\catacombae\hfsexplorer\gui\*.java ..\src\org\catacombae\hfsexplorer\testcode\*.java ..\src\org\catacombae\hfsexplorer\types\*.java ..\src\org\catacombae\hfsexplorer\win32\*.java
+set SOURCEFILES=..\src\org\catacombae\hfsexplorer\*.java ..\src\org\catacombae\hfsexplorer\gui\*.java ..\src\org\catacombae\hfsexplorer\partitioning\*.java ..\src\org\catacombae\hfsexplorer\testcode\*.java ..\src\org\catacombae\hfsexplorer\types\*.java ..\src\org\catacombae\hfsexplorer\win32\*.java
 set CLASSPATH=..\lib\swing-layout-1.0.1.jar;..\lib\hfsx_dmglib.jar
 
 rmdir /s /q doc.~
