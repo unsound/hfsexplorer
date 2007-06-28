6f66b76e6c99bcc701686a95cb3266b51330f5a5
Erik Larsson <catacombae@gmail.com>
2007-06-28 20:13:46 +0000

    Added a few more license headers and bumped version number to 0.18.

--- src/org/catacombae/hfsexplorer/win32/WritableWin32File.java
+++ src/org/catacombae/hfsexplorer/win32/WritableWin32File.java
@@ -1,7 +1,28 @@
+/*-
+ * Copyright (C) 2007 Erik Larsson
+ * 
+ * All rights reserved.
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA
+ */
+
 package org.catacombae.hfsexplorer.win32;
 import org.catacombae.hfsexplorer.*;
 
-/** BUG: Writing at the end of a file will always expand it to fit the sector size! Not very good. Fixit. */
+/** BUG: Writing at the end of a file will always expand it to fit the sector size! Not very good. Fixit.
+    Note: Is this fixed? */
 
 public class WritableWin32File extends WindowsLowLevelIO implements WritableLowLevelFile {
     private byte[] sectorBuffer;
