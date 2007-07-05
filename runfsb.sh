f48e159d879918da0c957750427140ebd495e778
Erik Larsson <catacombae@gmail.com>
2007-07-05 08:54:36 +0000

    Fixed up runfsb.sh after latest reorganization.

--- runfsb.sh
+++ runfsb.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-java -cp lib/hfsx.jar:lib/swing-layout-1.0.1.jar:lib/hfsx_dmglib.jar org.catacombae.hfsexplorer.FileSystemBrowserWindow $1 $2 $3 $4
+java -cp dist/lib/hfsx.jar:dist/lib/swing-layout-1.0.1.jar:dist/lib/hfsx_dmglib.jar org.catacombae.hfsexplorer.FileSystemBrowserWindow $1 $2 $3 $4
