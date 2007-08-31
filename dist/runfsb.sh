b17bddeebea90a2f49b099e53196070882651b53
Erik Larsson <catacombae@gmail.com>
2007-08-31 08:56:19 +0000

    Fixed bug in runfsb.sh (did not pass enough arguments on to 'java').

--- dist/runfsb.sh
+++ dist/runfsb.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-java -cp lib/hfsx.jar:lib/hfsx_dmglib.jar:lib/swing-layout-1.0.1.jar:lib/apache-ant-1.7.0-bzip2.jar:lib/iharder-base64.jar org.catacombae.hfsexplorer.FileSystemBrowserWindow $1 $2 $3 $4
+LIB=lib
+java -cp $LIB/hfsx.jar:$LIB/hfsx_dmglib.jar:$LIB/swing-layout-1.0.1.jar:$LIB/apache-ant-1.7.0-bzip2.jar:$LIB/iharder-base64.jar org.catacombae.hfsexplorer.FileSystemBrowserWindow $1 $2 $3 $4 $5 $6 $7 $8 $9
--- runfsb.sh
+++ runfsb.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-java -cp dist/lib/hfsx.jar:dist/lib/swing-layout-1.0.1.jar:dist/lib/hfsx_dmglib.jar org.catacombae.hfsexplorer.FileSystemBrowserWindow $1 $2 $3 $4
+LIB=dist/lib
+java -cp $LIB/hfsx.jar:$LIB/hfsx_dmglib.jar:$LIB/swing-layout-1.0.1.jar:$LIB/apache-ant-1.7.0-bzip2.jar:$LIB/iharder-base64.jar org.catacombae.hfsexplorer.FileSystemBrowserWindow $1 $2 $3 $4 $5 $6 $7 $8 $9
