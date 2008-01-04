e04ece2bc1b4a288be819fefedff53adbc641b38
Erik Larsson <catacombae@gmail.com>
2008-01-04 09:09:06 +0000

    Rebuild after reorganization.

--- src/org/catacombae/hfsexplorer/BuildNumber.java
+++ src/org/catacombae/hfsexplorer/BuildNumber.java
@@ -19,6 +19,6 @@ package org.catacombae.hfsexplorer;
 
 public class BuildNumber {
     //[BuildEnumerator:Opening] WARNING: The following lines are managed by an external program. Do NOT change.
-    public static final long BUILD_NUMBER = 352L;
+    public static final long BUILD_NUMBER = 353L;
     //[BuildEnumerator:Closing] The lines managed by an external program end here.
 }
