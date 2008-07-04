3cc6447a01c78ec1132b9339d3993834ec217943
Erik Larsson <catacombae@gmail.com>
2008-07-04 03:50:15 +0000

    Added Visual C++ support for buildllio scripts.

--- buildllio.bat
+++ buildllio.bat
@@ -1,10 +1,60 @@
 @echo off
-set SOURCE_FILES="%~dp0src.win32\llio_common.c" "%~dp0src.win32\llio.c" "%~dp0src.win32\org_catacombae_hfsexplorer_win32_WritableWin32File.c"
+setlocal
+
+:vars
+echo Calling common_vars
+call "%~dp0common_vars.bat"
+echo Finished calling common_vars
+set COMPILER=%1
+set ARCHITECTURE=%2
+set LLIO_SOURCEDIR=%~dp0\src.win32\llio
+
+:start
+if "%COMPILER%"=="gcc" goto build
+if "%COMPILER%"=="vc" goto build
+echo You must specify which compiler to use ("gcc" or "vc")...
+goto end
+
+:build
+
+echo Building all classes to make sure all is well...
 call "%~dp0\buildall.bat"
+if not "%ERRORLEVEL%"=="0" goto error
+
 echo Creating header file for org.catacombae.hfsexplorer.win32.WindowsLowLevelIO...
-javah -jni -classpath "%~dp0\build.~" -d "%~dp0\src.win32" org.catacombae.hfsexplorer.win32.WindowsLowLevelIO
+javah -jni -classpath "%CV_JAVA_BUILD_DIR%" -d "%LLIO_SOURCEDIR%" org.catacombae.hfsexplorer.win32.WindowsLowLevelIO
+if not "%ERRORLEVEL%"=="0" goto error
+
 echo Creating header file for org.catacombae.hfsexplorer.win32.WritableWin32File...
-javah -jni -classpath "%~dp0\build.~" -d "%~dp0\src.win32" org.catacombae.hfsexplorer.win32.WritableWin32File
-echo Compiling with gcc...
-call "%~dp0\buildllio_compile.bat"
+javah -jni -classpath "%CV_JAVA_BUILD_DIR%" -d "%LLIO_SOURCEDIR%" org.catacombae.hfsexplorer.win32.WritableWin32File
+if not "%ERRORLEVEL%"=="0" goto error
+
+if "%COMPILER%"=="gcc" goto gcc_compile
+if "%COMPILER%"=="vc" goto vc_compile
+goto error
+
+:gcc_compile
+
+echo Compiling with GCC...
+call "%~dp0\buildllio_compile.bat" gcc "%ARCHITECTURE%"
+if not "%ERRORLEVEL%"=="0" goto error
+goto completed
+
+:vc_compile
+
+echo Compiling with Visual C++...
+call "%~dp0\buildllio_compile.bat" vc "%ARCHITECTURE%"
+if not "%ERRORLEVEL%"=="0" goto error
+goto completed
+
+:error
+echo There were errors!
+goto end
+
+:completed
 echo Done!
+goto end
+
+:end
+
+endlocal
--- buildllio_compile.bat
+++ buildllio_compile.bat
@@ -1,2 +1,60 @@
 @echo off
-gcc -Wall -D_JNI_IMPLEMENTATION_ -Wl,--kill-at -shared %SOURCE_FILES% -o "%~dp0dist\llio.dll"
+
+setlocal
+
+set TARGET_DLL=%~dp0dist\llio.dll
+set SOURCE_DIR=%~dp0src.win32\llio
+set SOURCE_FILES="%SOURCE_DIR%\llio_common.c" "%SOURCE_DIR%\org_catacombae_hfsexplorer_win32_WindowsLowLevelIO.c" "%SOURCE_DIR%\org_catacombae_hfsexplorer_win32_WritableWin32File.c"
+
+:start
+
+if "%1"=="gcc" goto compile_gcc
+if "%1"=="vc" goto compile_vc
+
+goto printusage
+
+:printusage
+echo usage: %0 [gcc^|vc]
+goto end
+
+:compile_gcc
+echo Compiling with gcc...
+gcc -Wall -D_JNI_IMPLEMENTATION_ -Wl,--kill-at -shared %SOURCE_FILES% -o "%TARGET_DLL%"
+if not "%ERRORLEVEL%"=="0" goto error
+goto completed
+
+
+:compile_vc
+if "%JAVA_HOME%"=="" echo JAVA_HOME environment variable not defined! & goto error
+set JDK_PATH=%JAVA_HOME%
+set BUILD_DIR=%~dp0build.~
+
+echo Cleaning build dir...
+if exist "%BUILD_DIR%" rmdir /s /q "%BUILD_DIR%"
+if exist "%BUILD_DIR%" echo Could not clean build dir! & goto error
+mkdir "%BUILD_DIR%"
+if not "%ERRORLEVEL%"=="0" goto error
+
+echo Compiling with Visual C++...
+cl -c "/Fo%BUILD_DIR%\\" /W3 "/I%JDK_PATH%\include" "/I%JDK_PATH%\include\win32" "%SOURCE_DIR%\*.c"
+set EXIT_CODE=%ERRORLEVEL%
+if not "%EXIT_CODE%"=="0" goto error
+
+echo Linking...
+REM "/libpath:%JDK_PATH%\lib" 
+link /dll "/out:%TARGET_DLL%" "/implib:%BUILD_DIR%\llio.lib" "/pdb:%BUILD_DIR%\llio.pdb" "%BUILD_DIR%\*.obj"
+set EXIT_CODE=%ERRORLEVEL%
+if not "%EXIT_CODE%"=="0" goto error
+goto completed
+
+:completed
+echo Done!
+goto end
+
+:error
+echo There were errors...
+goto end
+
+:end
+
+endlocal
