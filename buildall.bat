00fccc267deaba848770ef16321ee37ff73373af
Erik Larsson <catacombae@gmail.com>
2008-08-26 15:16:36 +0000

    Possibly finalized FileSystemBrowser to a point where it's usable.
    Fixed the populateTreeNodesFromContents method to only update those that
    need to be updated, only remove those that need to be removed, ...
    
    I may have finally completed the new FileSystemBrowser to a version 1.0
    or something...

--- buildall.bat
+++ buildall.bat
@@ -3,27 +3,27 @@
 setlocal
 pushd %~dp0
 
-call "%~dp0common_vars.bat"
-set LIBRARY_PATH=%~dp0lib
-set JARFILE_PATH=%~dp0dist\lib
-set JARFILENAME=hfsx.jar
-set CLASSPATH="%~dp0build.~";"%~dp0dist\lib\catacombae_io.jar";"%~dp0dist\lib\swing-layout-1.0.3.jar";"%~dp0dist\lib\hfsx_dmglib.jar";"%~dp0lib\AppleJavaExtensions.jar";"%~dp0lib\java_awt_Desktop.jar"
+REM call "%~dp0common_vars.bat"
+REM set LIBRARY_PATH=%~dp0lib
+REM set JARFILE_PATH=%~dp0dist\lib
+REM set JARFILENAME=hfsx.jar
+REM set BUILD_CLASSPATH="%~dp0build.~";"%~dp0dist\lib\catacombae_io.jar";"%~dp0dist\lib\swing-layout-1.0.3.jar";"%~dp0dist\lib\hfsx_dmglib.jar";"%~dp0lib\AppleJavaExtensions.jar";"%~dp0lib\java_awt_Desktop.jar"
 set SOURCES_DIR=%~dp0src
-set BUILD_DIR=%CV_JAVA_BUILD_DIR%
-set RESOURCE_SRC_DIR=%~dp0resource
-set RESOURCE_DST_DIR=%BUILD_DIR%\res
+REM set BUILD_DIR=%CV_JAVA_BUILD_DIR%
+REM set RESOURCE_SRC_DIR=%~dp0resource
+REM set RESOURCE_DST_DIR=%BUILD_DIR%\res
 set BUILDENUM_CP="%~dp0lib\buildenumerator.jar"
-set MANIFEST=%~dp0src.META-INF\MANIFEST.MF
+REM set MANIFEST=%~dp0src.META-INF\MANIFEST.MF
 
-echo Removing all class files...
-if exist "%BUILD_DIR%" rmdir /s /q "%BUILD_DIR%"
-if not "%ERRORLEVEL%"=="0" goto error
-mkdir "%BUILD_DIR%"
-if not "%ERRORLEVEL%"=="0" goto error
+REM echo REMoving all class files...
+REM if exist "%BUILD_DIR%" rmdir /s /q "%BUILD_DIR%"
+REM if not "%ERRORLEVEL%"=="0" goto error
+REM mkdir "%BUILD_DIR%"
+REM if not "%ERRORLEVEL%"=="0" goto error
 
-echo Copying resources to classpath...
-mkdir "%RESOURCE_DST_DIR%"
-copy /y \\.\\"%RESOURCE_SRC_DIR%\*.png" "%RESOURCE_DST_DIR%" > NUL:
+REM echo Copying resources to classpath...
+REM mkdir "%RESOURCE_DST_DIR%"
+REM copy /y \\.\\"%RESOURCE_SRC_DIR%\*.png" "%RESOURCE_DST_DIR%" > NUL:
 
 echo Incrementing build number...
 java -cp %BUILDENUM_CP% BuildEnumerator "%SOURCES_DIR%\org\catacombae\hfsexplorer\BuildNumber.java" 1
@@ -32,40 +32,9 @@ REM Found out the trick for wildcard expansion of
 REM spaced file names! (enclosed within "") Just 
 REM prepend \\.\\ (\\.\ really, but last \ needed for ")
 
-echo Compiling org.catacombae.hfsexplorer...
-javac -target 1.5 -source 1.5 -cp %CLASSPATH% -sourcepath "%SOURCES_DIR%" -d "%BUILD_DIR%" -Xlint:unchecked \\.\\"%SOURCES_DIR%\org\catacombae\hfsexplorer\*.java"
-set JAVAC_EXIT_CODE=%ERRORLEVEL%
-if not "%JAVAC_EXIT_CODE%"=="0" goto error
-
-echo Compiling org.catacombae.hfsexplorer.gui...
-javac -target 1.5 -source 1.5 -cp %CLASSPATH% -sourcepath "%SOURCES_DIR%" -d "%BUILD_DIR%" -Xlint:unchecked \\.\\"%SOURCES_DIR%\org\catacombae\hfsexplorer\gui\*.java"
-set JAVAC_EXIT_CODE=%ERRORLEVEL%
-if not "%JAVAC_EXIT_CODE%"=="0" goto error
-
-echo Compiling org.catacombae.hfsexplorer.partitioning...
-javac -target 1.5 -source 1.5 -cp %CLASSPATH% -sourcepath "%SOURCES_DIR%" -d "%BUILD_DIR%" -Xlint:unchecked \\.\\"%SOURCES_DIR%\org\catacombae\hfsexplorer\partitioning\*.java"
-set JAVAC_EXIT_CODE=%ERRORLEVEL%
-if not "%JAVAC_EXIT_CODE%"=="0" goto error
-
-echo Compiling org.catacombae.hfsexplorer.testcode...
-javac -target 1.5 -source 1.5 -cp %CLASSPATH% -sourcepath "%SOURCES_DIR%" -d "%BUILD_DIR%" -Xlint:unchecked \\.\\"%SOURCES_DIR%\org\catacombae\hfsexplorer\testcode\*.java"
-set JAVAC_EXIT_CODE=%ERRORLEVEL%
-if not "%JAVAC_EXIT_CODE%"=="0" goto error
-
-echo Compiling org.catacombae.hfsexplorer.types...
-javac -target 1.5 -source 1.5 -cp %CLASSPATH% -sourcepath "%SOURCES_DIR%" -d "%BUILD_DIR%" -Xlint:unchecked \\.\\"%SOURCES_DIR%\org\catacombae\hfsexplorer\types\*.java"
-set JAVAC_EXIT_CODE=%ERRORLEVEL%
-if not "%JAVAC_EXIT_CODE%"=="0" goto error
-
-echo Compiling org.catacombae.hfsexplorer.win32...
-javac -target 1.5 -source 1.5 -cp %CLASSPATH% -sourcepath "%SOURCES_DIR%" -d "%BUILD_DIR%" -Xlint:unchecked \\.\\"%SOURCES_DIR%\org\catacombae\hfsexplorer\win32\*.java"
-set JAVAC_EXIT_CODE=%ERRORLEVEL%
-if not "%JAVAC_EXIT_CODE%"=="0" goto error
-
-echo Building jar-file...
-if not exist "%JARFILE_PATH%" mkdir "%JARFILE_PATH%"
-jar cvfm "%JARFILE_PATH%\%JARFILENAME%" "%MANIFEST%" -C "%BUILD_DIR%" . >NUL:
-if "%ERRORLEVEL%"=="0" (echo Done!) else echo Problems while building jar-file...
+echo Building with ant...
+call ant build-all
+if not "%ERRORLEVEL%"=="0" goto error
 
 goto end
 
