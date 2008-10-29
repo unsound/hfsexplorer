cf25f9606c8f74458be81539fab01eb1b92a0e3d
Erik Larsson <catacombae@gmail.com>
2008-10-29 07:02:30 +0000

    Touched up the GUI for FSEntrySummaryPanel for a nicer look.

--- src/org/catacombae/hfsexplorer/gui/FSEntrySummaryPanel.java
+++ src/org/catacombae/hfsexplorer/gui/FSEntrySummaryPanel.java
@@ -121,29 +121,13 @@ public class FSEntrySummaryPanel extends javax.swing.JPanel {
         sizeField.setText(sizeString);
 
         FSAttributes attrs = entry.getAttributes();
-        if(attrs.hasPOSIXFileAttributes())
-            setPOSIXFileAttributes(attrs.getPOSIXFileAttributes());
+        if(attrs.hasPOSIXFileAttributes()) {
+            POSIXAttributesPanel attributesPanel =
+                    new POSIXAttributesPanel(attrs.getPOSIXFileAttributes());
+            extendedInfoStackPanel.add(attributesPanel);
+        }
     }
-
-    private void setPOSIXFileAttributes(POSIXFileAttributes attrs) {
-        permissionStringField.setText(attrs.getPermissionString());
-
-        userReadBox.setSelected(attrs.canUserRead());
-        userWriteBox.setSelected(attrs.canUserWrite());
-        userExecuteBox.setSelected(attrs.canUserExecute());
-        groupReadBox.setSelected(attrs.canGroupRead());
-        groupWriteBox.setSelected(attrs.canGroupWrite());
-        groupExecuteBox.setSelected(attrs.canGroupExecute());
-        otherReadBox.setSelected(attrs.canOthersRead());
-        otherWriteBox.setSelected(attrs.canOthersWrite());
-        otherExecuteBox.setSelected(attrs.canOthersExecute());
-        
-        ownerIDField.setText(attrs.getUserID() + "");
-        groupIDField.setText(attrs.getGroupID() + "");
-
-
-    }
-
+    
     /** This method is called from within the constructor to
      * initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is
@@ -160,217 +144,49 @@ public class FSEntrySummaryPanel extends javax.swing.JPanel {
         jLabel3 = new javax.swing.JLabel();
         sizeField = new javax.swing.JTextField();
         jSeparator1 = new javax.swing.JSeparator();
-        posixPermissionsPanel = new javax.swing.JPanel();
-        jLabel4 = new javax.swing.JLabel();
-        userReadBox = new javax.swing.JCheckBox();
-        userWriteBox = new javax.swing.JCheckBox();
-        userExecuteBox = new javax.swing.JCheckBox();
-        userLabel = new javax.swing.JLabel();
-        groupLabel = new javax.swing.JLabel();
-        groupReadBox = new javax.swing.JCheckBox();
-        groupWriteBox = new javax.swing.JCheckBox();
-        groupExecuteBox = new javax.swing.JCheckBox();
-        otherLabel = new javax.swing.JLabel();
-        otherReadBox = new javax.swing.JCheckBox();
-        otherWriteBox = new javax.swing.JCheckBox();
-        otherExecuteBox = new javax.swing.JCheckBox();
-        ownerIDLabel = new javax.swing.JLabel();
-        ownerIDField = new javax.swing.JTextField();
-        groupIDLabel = new javax.swing.JLabel();
-        groupIDField = new javax.swing.JTextField();
-        jCheckBox10 = new javax.swing.JCheckBox();
-        jCheckBox11 = new javax.swing.JCheckBox();
-        jCheckBox12 = new javax.swing.JCheckBox();
-        jLabel10 = new javax.swing.JLabel();
-        permissionStringField = new javax.swing.JTextField();
+        extendedInfoStackPanel = new javax.swing.JPanel();
 
         jLabel1.setText("Name:");
 
+        nameField.setEditable(false);
         nameField.setText("jTextField1");
+        nameField.setOpaque(false);
 
         jLabel2.setText("Type:");
 
+        typeField.setEditable(false);
         typeField.setText("jTextField2");
+        typeField.setOpaque(false);
 
         jLabel3.setText("Size:");
 
+        sizeField.setEditable(false);
         sizeField.setText("jTextField3");
+        sizeField.setOpaque(false);
 
-        jLabel4.setText("POSIX permissions:");
-
-        userReadBox.setText("Read");
-
-        userWriteBox.setText("Write");
-
-        userExecuteBox.setText("Execute");
-
-        userLabel.setText("User:");
-
-        groupLabel.setText("Group:");
-
-        groupReadBox.setText("Read");
-
-        groupWriteBox.setText("Write");
-
-        groupExecuteBox.setText("Execute");
-
-        otherLabel.setText("Other:");
-
-        otherReadBox.setText("Read");
-
-        otherWriteBox.setText("Write");
-
-        otherExecuteBox.setText("Execute");
-
-        ownerIDLabel.setText("Owner ID:");
-
-        ownerIDField.setEditable(false);
-        ownerIDField.setText("jTextField4");
-        ownerIDField.setOpaque(false);
-
-        groupIDLabel.setText("Group ID:");
-
-        groupIDField.setEditable(false);
-        groupIDField.setText("jTextField5");
-        groupIDField.setOpaque(false);
-
-        jCheckBox10.setText("Set user ID on execution");
-
-        jCheckBox11.setText("Set group ID on execution");
-
-        jCheckBox12.setText("Sticky bit");
-
-        jLabel10.setText("Permission string:");
-
-        permissionStringField.setEditable(false);
-        permissionStringField.setText("jTextField6");
-        permissionStringField.setOpaque(false);
-
-        org.jdesktop.layout.GroupLayout posixPermissionsPanelLayout = new org.jdesktop.layout.GroupLayout(posixPermissionsPanel);
-        posixPermissionsPanel.setLayout(posixPermissionsPanelLayout);
-        posixPermissionsPanelLayout.setHorizontalGroup(
-            posixPermissionsPanelLayout.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING)
-            .add(posixPermissionsPanelLayout.createSequentialGroup()
-                .add(posixPermissionsPanelLayout.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING)
-                    .add(jLabel4)
-                    .add(posixPermissionsPanelLayout.createSequentialGroup()
-                        .addContainerGap()
-                        .add(jLabel10)
-                        .addPreferredGap(org.jdesktop.layout.LayoutStyle.RELATED)
-                        .add(permissionStringField, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, 191, Short.MAX_VALUE)))
-                .addContainerGap())
-            .add(posixPermissionsPanelLayout.createSequentialGroup()
-                .addContainerGap()
-                .add(posixPermissionsPanelLayout.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING)
-                    .add(posixPermissionsPanelLayout.createSequentialGroup()
-                        .addPreferredGap(org.jdesktop.layout.LayoutStyle.RELATED, 3, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE)
-                        .add(posixPermissionsPanelLayout.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING)
-                            .add(groupLabel)
-                            .add(userLabel)
-                            .add(otherLabel))
-                        .add(18, 18, 18)
-                        .add(posixPermissionsPanelLayout.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING)
-                            .add(posixPermissionsPanelLayout.createSequentialGroup()
-                                .add(otherReadBox)
-                                .addPreferredGap(org.jdesktop.layout.LayoutStyle.RELATED)
-                                .add(otherWriteBox)
-                                .addPreferredGap(org.jdesktop.layout.LayoutStyle.RELATED)
-                                .add(otherExecuteBox))
-                            .add(posixPermissionsPanelLayout.createSequentialGroup()
-                                .add(groupReadBox)
-                                .addPreferredGap(org.jdesktop.layout.LayoutStyle.RELATED)
-                                .add(groupWriteBox)
-                                .addPreferredGap(org.jdesktop.layout.LayoutStyle.RELATED)
-                                .add(groupExecuteBox))
-                            .add(posixPermissionsPanelLayout.createSequentialGroup()
-                                .add(userReadBox)
-                                .addPreferredGap(org.jdesktop.layout.LayoutStyle.RELATED)
-                                .add(userWriteBox)
-                                .addPreferredGap(org.jdesktop.layout.LayoutStyle.RELATED)
-                                .add(userExecuteBox))))
-                    .add(jCheckBox10)
-                    .add(jCheckBox11)
-                    .add(jCheckBox12)
-                    .add(posixPermissionsPanelLayout.createSequentialGroup()
-                        .addPreferredGap(org.jdesktop.layout.LayoutStyle.RELATED)
-                        .add(posixPermissionsPanelLayout.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING)
-                            .add(ownerIDLabel)
-                            .add(groupIDLabel))
-                        .addPreferredGap(org.jdesktop.layout.LayoutStyle.RELATED)
-                        .add(posixPermissionsPanelLayout.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING)
-                            .add(groupIDField, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, 249, Short.MAX_VALUE)
-                            .add(ownerIDField, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, 249, Short.MAX_VALUE))))
-                .add(17, 17, 17))
-        );
-        posixPermissionsPanelLayout.setVerticalGroup(
-            posixPermissionsPanelLayout.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING)
-            .add(posixPermissionsPanelLayout.createSequentialGroup()
-                .add(jLabel4)
-                .addPreferredGap(org.jdesktop.layout.LayoutStyle.RELATED)
-                .add(posixPermissionsPanelLayout.createParallelGroup(org.jdesktop.layout.GroupLayout.BASELINE)
-                    .add(jLabel10)
-                    .add(permissionStringField, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE))
-                .addPreferredGap(org.jdesktop.layout.LayoutStyle.RELATED)
-                .add(posixPermissionsPanelLayout.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING)
-                    .add(posixPermissionsPanelLayout.createSequentialGroup()
-                        .add(userLabel)
-                        .addPreferredGap(org.jdesktop.layout.LayoutStyle.RELATED)
-                        .add(groupLabel)
-                        .addPreferredGap(org.jdesktop.layout.LayoutStyle.RELATED)
-                        .add(otherLabel))
-                    .add(posixPermissionsPanelLayout.createSequentialGroup()
-                        .add(posixPermissionsPanelLayout.createParallelGroup(org.jdesktop.layout.GroupLayout.BASELINE)
-                            .add(userReadBox)
-                            .add(userWriteBox)
-                            .add(userExecuteBox))
-                        .addPreferredGap(org.jdesktop.layout.LayoutStyle.RELATED)
-                        .add(posixPermissionsPanelLayout.createParallelGroup(org.jdesktop.layout.GroupLayout.BASELINE)
-                            .add(groupReadBox)
-                            .add(groupWriteBox)
-                            .add(groupExecuteBox))
-                        .addPreferredGap(org.jdesktop.layout.LayoutStyle.RELATED)
-                        .add(posixPermissionsPanelLayout.createParallelGroup(org.jdesktop.layout.GroupLayout.BASELINE)
-                            .add(otherReadBox)
-                            .add(otherWriteBox)
-                            .add(otherExecuteBox))))
-                .addPreferredGap(org.jdesktop.layout.LayoutStyle.UNRELATED)
-                .add(jCheckBox10)
-                .addPreferredGap(org.jdesktop.layout.LayoutStyle.RELATED)
-                .add(jCheckBox11)
-                .addPreferredGap(org.jdesktop.layout.LayoutStyle.RELATED)
-                .add(jCheckBox12)
-                .addPreferredGap(org.jdesktop.layout.LayoutStyle.RELATED)
-                .add(posixPermissionsPanelLayout.createParallelGroup(org.jdesktop.layout.GroupLayout.BASELINE)
-                    .add(ownerIDLabel)
-                    .add(ownerIDField, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE))
-                .addPreferredGap(org.jdesktop.layout.LayoutStyle.RELATED)
-                .add(posixPermissionsPanelLayout.createParallelGroup(org.jdesktop.layout.GroupLayout.BASELINE)
-                    .add(groupIDLabel)
-                    .add(groupIDField, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE))
-                .addContainerGap(org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-        );
+        extendedInfoStackPanel.setLayout(new javax.swing.BoxLayout(extendedInfoStackPanel, javax.swing.BoxLayout.PAGE_AXIS));
 
         org.jdesktop.layout.GroupLayout layout = new org.jdesktop.layout.GroupLayout(this);
         this.setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING)
-            .add(layout.createSequentialGroup()
+            .add(org.jdesktop.layout.GroupLayout.TRAILING, layout.createSequentialGroup()
                 .addContainerGap()
-                .add(layout.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING)
-                    .add(jSeparator1, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, 355, Short.MAX_VALUE)
-                    .add(layout.createSequentialGroup()
+                .add(layout.createParallelGroup(org.jdesktop.layout.GroupLayout.TRAILING)
+                    .add(org.jdesktop.layout.GroupLayout.LEADING, extendedInfoStackPanel, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, 350, Short.MAX_VALUE)
+                    .add(org.jdesktop.layout.GroupLayout.LEADING, jSeparator1, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, 350, Short.MAX_VALUE)
+                    .add(org.jdesktop.layout.GroupLayout.LEADING, layout.createSequentialGroup()
                         .add(jLabel1)
                         .addPreferredGap(org.jdesktop.layout.LayoutStyle.RELATED)
-                        .add(nameField, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, 305, Short.MAX_VALUE))
-                    .add(layout.createSequentialGroup()
+                        .add(nameField, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, 300, Short.MAX_VALUE))
+                    .add(org.jdesktop.layout.GroupLayout.LEADING, layout.createSequentialGroup()
                         .add(jLabel2)
                         .addPreferredGap(org.jdesktop.layout.LayoutStyle.RELATED)
-                        .add(typeField, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, 311, Short.MAX_VALUE))
-                    .add(layout.createSequentialGroup()
+                        .add(typeField, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, 306, Short.MAX_VALUE))
+                    .add(org.jdesktop.layout.GroupLayout.LEADING, layout.createSequentialGroup()
                         .add(jLabel3)
                         .addPreferredGap(org.jdesktop.layout.LayoutStyle.RELATED)
-                        .add(sizeField, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, 316, Short.MAX_VALUE))
-                    .add(org.jdesktop.layout.GroupLayout.TRAILING, posixPermissionsPanel, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                        .add(sizeField, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, 311, Short.MAX_VALUE)))
                 .addContainerGap())
         );
         layout.setVerticalGroup(
@@ -390,44 +206,22 @@ public class FSEntrySummaryPanel extends javax.swing.JPanel {
                     .add(sizeField, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE))
                 .addPreferredGap(org.jdesktop.layout.LayoutStyle.RELATED)
                 .add(jSeparator1, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(org.jdesktop.layout.LayoutStyle.RELATED, 364, Short.MAX_VALUE)
-                .add(posixPermissionsPanel, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(org.jdesktop.layout.LayoutStyle.RELATED)
+                .add(extendedInfoStackPanel, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, 107, Short.MAX_VALUE)
                 .addContainerGap())
         );
     }// </editor-fold>//GEN-END:initComponents
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JCheckBox groupExecuteBox;
-    private javax.swing.JTextField groupIDField;
-    private javax.swing.JLabel groupIDLabel;
-    private javax.swing.JLabel groupLabel;
-    private javax.swing.JCheckBox groupReadBox;
-    private javax.swing.JCheckBox groupWriteBox;
-    private javax.swing.JCheckBox jCheckBox10;
-    private javax.swing.JCheckBox jCheckBox11;
-    private javax.swing.JCheckBox jCheckBox12;
+    private javax.swing.JPanel extendedInfoStackPanel;
     private javax.swing.JLabel jLabel1;
-    private javax.swing.JLabel jLabel10;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
-    private javax.swing.JLabel jLabel4;
     private javax.swing.JSeparator jSeparator1;
     private javax.swing.JTextField nameField;
-    private javax.swing.JCheckBox otherExecuteBox;
-    private javax.swing.JLabel otherLabel;
-    private javax.swing.JCheckBox otherReadBox;
-    private javax.swing.JCheckBox otherWriteBox;
-    private javax.swing.JTextField ownerIDField;
-    private javax.swing.JLabel ownerIDLabel;
-    private javax.swing.JTextField permissionStringField;
-    private javax.swing.JPanel posixPermissionsPanel;
     private javax.swing.JTextField sizeField;
     private javax.swing.JTextField typeField;
-    private javax.swing.JCheckBox userExecuteBox;
-    private javax.swing.JLabel userLabel;
-    private javax.swing.JCheckBox userReadBox;
-    private javax.swing.JCheckBox userWriteBox;
     // End of variables declaration//GEN-END:variables
 
     private String getSizeString(long result) {
