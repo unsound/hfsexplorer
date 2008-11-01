8ea00a7621e5df9ce1b24bf8fc35c73456a8f494
Erik Larsson <catacombae@gmail.com>
2008-11-01 07:46:40 +0000

    Restructured the runfsb.sh scripts.

--- dist/runfsb.sh
+++ dist/runfsb.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 LIB=lib
-java -cp $LIB/hfsx.jar org.catacombae.hfsexplorer.FileSystemBrowserWindow $1 $2 $3 $4 $5 $6 $7 $8 $9
+java "-Xdock:name=HFSExplorer" -jar "$LIB/hfsx.jar" "$@"
--- runfsb.sh
+++ runfsb.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
-LIB=dist/lib
-java -cp $LIB/hfsx.jar:$LIB/hfsx_dmglib.jar:$LIB/swing-layout-1.0.1.jar:$LIB/apache-ant-1.7.0-bzip2.jar:$LIB/iharder-base64.jar org.catacombae.hfsexplorer.FileSystemBrowserWindow "$@"
+
+pushd dist > /dev/null
+./runfsb.sh "$@"
+popd > /dev/null
