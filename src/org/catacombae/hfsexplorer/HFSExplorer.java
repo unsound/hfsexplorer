aa8e6e4988150a79014e76fdf27e3fa273d088a0
Erik Larsson <catacombae@gmail.com>
2008-11-06 14:22:54 +0000

    Updated version number to 0.20.1 (build number 1000) and rebuilt binary.

--- src/org/catacombae/hfsexplorer/HFSExplorer.java
+++ src/org/catacombae/hfsexplorer/HFSExplorer.java
@@ -47,7 +47,7 @@ import org.catacombae.hfsexplorer.partitioning.APMPartition;
 
 @SuppressWarnings("deprecation") // TODO: Fix HFSExplorer so it doesn't use deprecated methods...
 public class HFSExplorer {
-    public static final String VERSION = "0.20";
+    public static final String VERSION = "0.20.1";
     public static final String COPYRIGHT = "Copyright \u00A9 Erik Larsson 2006-2008";
     public static final String[] NOTICES =
     {
