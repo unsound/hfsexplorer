10bff33101268e7b66dfd46716ed6ac6295cf02d
Erik Larsson <catacombae@gmail.com>
2008-11-07 03:31:50 +0000

    Added Mac OS X Dock icon for HFSExplorer.

--- dist/runfsb.sh
+++ dist/runfsb.sh
@@ -4,5 +4,6 @@ LIB=lib
 UNAME=`uname`
 if [ "${UNAME}" = "Darwin" ]; then
     DOCK_NAME="-Xdock:name=HFSExplorer"
+    DOCK_ICON="-Xdock:icon=res/icon.png"
 fi
-java ${DOCK_NAME:+"${DOCK_NAME}"} -cp "$LIB/hfsx.jar" org.catacombae.hfsexplorer.FileSystemBrowserWindow "$@"
+java ${DOCK_NAME:+"${DOCK_NAME}"} ${DOCK_ICON:+"${DOCK_ICON}"} -cp "$LIB/hfsx.jar" org.catacombae.hfsexplorer.FileSystemBrowserWindow "$@"
