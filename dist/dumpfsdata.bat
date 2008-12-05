8de006448e04bc1ba681b75313490bb57eed374f
Erik Larsson <catacombae@gmail.com>
2008-12-05 06:34:56 +0000

    Create UnHFS startup script and fix dumpfsdata.bat after recent changes.

--- dist/dumpfsdata.bat
+++ dist/dumpfsdata.bat
@@ -2,6 +2,6 @@
 setlocal
 REM ** The local extension of the PATH environment variable is neccessary for java to find llio.dll
 PATH=%PATH%;%~dp0lib
-java -cp "%~dp0lib\hfsx.jar" org.catacombae.hfsexplorer.testcode.DumpFSInfo %1 %2 %3 %4 %5 %6 %7 %8 %9
+java -cp "%~dp0lib\hfsx.jar" org.catacombae.hfsexplorer.tools.DumpFSInfo %1 %2 %3 %4 %5 %6 %7 %8 %9
 endlocal
 pause
