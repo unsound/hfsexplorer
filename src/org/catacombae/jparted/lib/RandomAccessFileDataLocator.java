2c7390420bbf5a0cad16851ffa265c138cb1dea0
Erik Larsson <catacombae@gmail.com>
2009-09-25 07:30:06 +0200

    Made writable mode optional in RandomAccessFileDataLocator.
    Also added javadoc for various methods in RandomAccessFileDataLocator.

--- src/org/catacombae/jparted/lib/RandomAccessFileDataLocator.java
+++ src/org/catacombae/jparted/lib/RandomAccessFileDataLocator.java
@@ -32,29 +32,111 @@ import org.catacombae.io.FileStream;
 public class RandomAccessFileDataLocator extends DataLocator {
     private final File file;
     private final Long pos, len;
+    private final boolean writable;
     
+    /**
+     * Creates a new DataLocator with a path to the backing file.
+     *
+     * @param pPath     path to the backing file.
+     */
     public RandomAccessFileDataLocator(String pPath) {
         this(new File(pPath));
     }
     
+    /**
+     * Creates a new DataLocator with a path to the backing file.
+     *
+     * @param pPath     path to the backing file.
+     * @param pWritable whether or not the data locator should be able to open
+     *                  writable streams.
+     */
+    public RandomAccessFileDataLocator(String pPath, boolean pWritable) {
+        this(new File(pPath), pWritable);
+    }
+
+    /**
+     * Creates a new DataLocator with a path to the backing file.
+     *
+     * @param pPath     path to the backing file.
+     * @param pPos      the start offset in the file for the data.
+     * @param pLen      the length of the data being addressed.
+     */
     public RandomAccessFileDataLocator(String pPath, long pPos, long pLen) {
-        this(new File(pPath), pPos, pLen);
+        this(new File(pPath), pPos, pLen, true);
     }
+
+    /**
+     * Creates a new DataLocator with a path to the backing file.
+     *
+     * @param pPath     path to the backing file.
+     * @param pPos      the start offset in the file for the data.
+     * @param pLen      the length of the data being addressed.
+     * @param pWritable whether or not the data locator should be able to open
+     *                  writable streams.
+     */
+    public RandomAccessFileDataLocator(String pPath, long pPos, long pLen,
+            boolean pWritable) {
+        this(new File(pPath), pPos, pLen, pWritable);
+    }
+
+    /**
+     * Creates a new DataLocator with File object pointing at the backing file.
+     * The DataLocator will be writable.
+     *
+     * @param pFile the backing file.
+     */
     public RandomAccessFileDataLocator(File pFile) {
-        this(pFile, null, null);
+        this(pFile, null, null, true);
+    }
+
+    /**
+     * Creates a new DataLocator with File object pointing at the backing file.
+     *
+     * @param pFile     the backing file.
+     * @param pWritable whether or not the data locator should be able to open
+     *                  writable streams.
+     */
+    public RandomAccessFileDataLocator(File pFile, boolean pWritable) {
+        this(pFile, null, null, pWritable);
     }
     
+    /**
+     * Creates a new DataLocator with File object pointing at the backing file.
+     *
+     * @param pFile     the backing file.
+     * @param pPos      the start offset in the file for the data.
+     * @param pLen      the length of the data being addressed.
+     */
     public RandomAccessFileDataLocator(File pFile, long pPos, long pLen) {
-        this(pFile, new Long(pPos), new Long(pLen));
+        this(pFile, new Long(pPos), new Long(pLen), true);
+    }
+
+    /**
+     * Creates a new DataLocator with File object pointing at the backing file.
+     *
+     * @param pFile     the backing file.
+     * @param pPos      the start offset in the file for the data.
+     * @param pLen      the length of the data being addressed.
+     * @param pWritable whether or not the data locator should be able to open
+     *                  writable streams.
+     */
+    public RandomAccessFileDataLocator(File pFile, long pPos, long pLen,
+            boolean pWritable) {
+        this(pFile, new Long(pPos), new Long(pLen), pWritable);
     }
     
-    private RandomAccessFileDataLocator(File pFile, Long pPos, Long pLen) {
+    private RandomAccessFileDataLocator(File pFile, Long pPos, Long pLen,
+            boolean writable) {
         if(!pFile.canRead())
             throw new RuntimeException("Can not read from file!");
+        if(writable && !pFile.canWrite())
+            throw new RuntimeException("Requested write mode but file is not " +
+                    "writable.");
 
         this.file = pFile;
         this.pos = pPos;
         this.len = pLen;
+        this.writable = writable;
     }
     
     @Override
@@ -77,6 +159,6 @@ public class RandomAccessFileDataLocator extends DataLocator {
     
     @Override
     public boolean isWritable() {
-        return true;
+        return writable;
     }    
 }
