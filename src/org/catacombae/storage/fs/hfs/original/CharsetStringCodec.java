099d061fb84b9ad6285ad4bd976691010c5b6035
Erik Larsson <catacombae@gmail.com>
2009-11-10 22:29:25 +0100

    Moved HFS-specific StringCodec classes to the proper package.

--- src/org/catacombae/storage/fs/hfs/original/CharsetStringCodec.java
+++ src/org/catacombae/storage/fs/hfs/original/CharsetStringCodec.java
@@ -15,7 +15,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-package org.catacombae.hfsexplorer.fs;
+package org.catacombae.storage.fs.hfs.original;
 
 import java.nio.ByteBuffer;
 import java.nio.CharBuffer;
