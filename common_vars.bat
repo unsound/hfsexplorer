abed9ad47c9b84414fd858600c07aaaadc31b7cc
Erik Larsson <catacombae@gmail.com>
2009-11-11 12:39:39 +0100

    More fixes to llio build scripts.

--- common_vars.bat
+++ common_vars.bat
@@ -1,4 +1,4 @@
 REM echo Entering common_vars
-set CV_JAVA_BUILD_DIR=%~dp0.build.java~
+set CV_JAVA_BUILD_DIR=%~dp0.antbuild~
 set CV_NATIVE_BUILD_DIR=%~dp0.build.native~
 REM echo Exiting common_vars
