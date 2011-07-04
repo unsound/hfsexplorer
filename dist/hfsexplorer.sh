2198a61bdbd8380703fd5962d5d20deed50733bf
Erik Larsson <catacombae@gmail.com>
2011-07-04 08:36:13 +0200

    Improved hfsexplorer.sh launcher script, making it location independent.
    This is achieved with the help of the 'dirname' utility (hopefully the
    client platform has this utility).

--- dist/hfsexplorer.sh
+++ dist/hfsexplorer.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
-LIB=lib
+BASEDIR="`dirname "$0"`"
+LIB="${BASEDIR}/lib"
 
 UNAME=`uname`
 if [ "${UNAME}" = "Darwin" ]; then
     DOCK_NAME="-Xdock:name=HFSExplorer"
-    DOCK_ICON="-Xdock:icon=res/icon.png"
+    DOCK_ICON="-Xdock:icon=${BASEDIR}/res/icon.png"
 fi
 java ${DOCK_NAME:+"${DOCK_NAME}"} ${DOCK_ICON:+"${DOCK_ICON}"} -cp "$LIB/hfsx.jar" org.catacombae.hfsexplorer.FileSystemBrowserWindow "$@"
