814ac31be581deb39e1245030f464bf3329e09f8
Erik Larsson <catacombae@gmail.com>
2014-08-24 13:02:49 +0200

    test.sh: Fix paths to .jar files.
    The paths to the .jar files "apache-ant-1.7.0-bzip2.jar" and
    "iharder-base64.jar" were specified as /lib/<jarfile>, but they are
    actually located in /dist/lib.

--- test.sh
+++ test.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-CLASSPATH="dist/lib/hfsx.jar":"dist/lib/swing-layout-1.0.3.jar":"dist/lib/hfsx_dmglib.jar":"lib/apache-ant-1.7.0-bzip2.jar":"lib/iharder-base64.jar":"dist/lib/catacombae_io.jar"
+CLASSPATH="dist/lib/hfsx.jar":"dist/lib/swing-layout-1.0.3.jar":"dist/lib/hfsx_dmglib.jar":"dist/lib/apache-ant-1.7.0-bzip2.jar":"dist/lib/iharder-base64.jar":"dist/lib/catacombae_io.jar"
 java -cp $CLASSPATH org.catacombae.hfsexplorer.$1 ${2:+"${2}"} ${3:+"$3"} ${4:+"$4"} ${5:+"$5"} ${6:+"$6"} ${7:+"$7"} ${8:+"$8"} ${9:+"$9"}
