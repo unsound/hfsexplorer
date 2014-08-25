52a360632614cb3fab0a37e828f658b90c66210d
Erik Larsson <catacombae@gmail.com>
2014-08-25 12:16:40 +0200

    HFSExplorer 0.22.1.
    - Update changelog, nsis script, sdic, launcher and build number /
      version in sources.
    - Update launcher binary with latest fixes + 0.22.1 version number.
    - Build number now derives directly from the version number. The old
      build number system is officially retired.

--- doc/hfsexplorer.nsi
+++ doc/hfsexplorer.nsi
@@ -2,7 +2,7 @@
 
 ; HM NIS Edit Wizard helper defines
 !define PRODUCT_NAME "HFSExplorer"
-!define PRODUCT_VERSION "0.22"
+!define PRODUCT_VERSION "0.22.1"
 !define PRODUCT_PUBLISHER "Catacombae Software"
 !define PRODUCT_WEB_SITE "http://www.catacombae.org/"
 !define PRODUCT_UNINST_KEY "Software\Microsoft\Windows\CurrentVersion\Uninstall\${PRODUCT_NAME}"
--- src/org/catacombae/hfsexplorer/HFSExplorer.java
+++ src/org/catacombae/hfsexplorer/HFSExplorer.java
@@ -54,7 +54,7 @@ import org.catacombae.hfs.HFSVolume;
 
 @SuppressWarnings("deprecation") // TODO: Fix HFSExplorer so it doesn't use deprecated methods...
 public class HFSExplorer {
-    public static final String VERSION = "0.22";
+    public static final String VERSION = "0.22.1";
     public static final String COPYRIGHT =
             "Copyright \u00A9 Erik Larsson 2006-2014";
     public static final String[] NOTICES = {
