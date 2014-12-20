ed58ae0478d238e2f7086fcbff7d76108a74d208
Erik Larsson <catacombae@gmail.com>
2014-12-20 09:12:25 +0100

    Add NetBeans project HFSExplorerNative.
    This project can be used to edit the Win32 sources of the llio native
    library and launcher executable.

---
+++ netbeans/HFSExplorerNative/nbproject/Makefile-Release.mk
@@ -0,0 +1,97 @@
+#
+# Generated Makefile - do not edit!
+#
+# Edit the Makefile in the project folder instead (../Makefile). Each target
+# has a -pre and a -post target defined where you can add customized code.
+#
+# This makefile implements configuration specific macros and targets.
+
+
+# Environment
+MKDIR=mkdir
+CP=cp
+CCADMIN=CCadmin
+RANLIB=ranlib
+CC=gcc
+CCC=g++
+CXX=g++
+FC=g77
+AS=
+
+# Macros
+CND_PLATFORM=MinGW-Windows
+CND_CONF=Release
+CND_DISTDIR=dist
+
+# Include project Makefile
+include Makefile
+
+# Object Directory
+OBJECTDIR=build/${CND_CONF}/${CND_PLATFORM}
+
+# Object Files
+OBJECTFILES= \
+	${OBJECTDIR}/_ext/_DOTDOT/_DOTDOT/src/win32/llio/llio_common.o \
+	${OBJECTDIR}/_ext/_DOTDOT/_DOTDOT/src/win32/launcher/launcher.o \
+	${OBJECTDIR}/_ext/_DOTDOT/_DOTDOT/src/win32/llio/org_catacombae_storage_io_win32_Win32FileStream.o \
+	${OBJECTDIR}/_ext/_DOTDOT/_DOTDOT/src/win32/llio/org_catacombae_storage_io_win32_ReadableWin32FileStream.o
+
+# C Compiler Flags
+CFLAGS=
+
+# CC Compiler Flags
+CCFLAGS=
+CXXFLAGS=
+
+# Fortran Compiler Flags
+FFLAGS=
+
+# Assembler Flags
+ASFLAGS=
+
+# Link Libraries and Options
+LDLIBSOPTIONS=
+
+# Build Targets
+.build-conf: ${BUILD_SUBPROJECTS}
+	${MAKE}  -f nbproject/Makefile-Release.mk dist/Release/MinGW-Windows/libHFSExplorerNative.dll
+
+dist/Release/MinGW-Windows/libHFSExplorerNative.dll: ${OBJECTFILES}
+	${MKDIR} -p dist/Release/MinGW-Windows
+	${LINK.cc} -shared -o ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/libHFSExplorerNative.dll ${OBJECTFILES} ${LDLIBSOPTIONS} 
+
+${OBJECTDIR}/_ext/_DOTDOT/_DOTDOT/src/win32/llio/llio_common.o: nbproject/Makefile-${CND_CONF}.mk ../../src/win32/llio/llio_common.c 
+	${MKDIR} -p ${OBJECTDIR}/_ext/_DOTDOT/_DOTDOT/src/win32/llio
+	${RM} $@.d
+	$(COMPILE.c) -O2  -MMD -MP -MF $@.d -o ${OBJECTDIR}/_ext/_DOTDOT/_DOTDOT/src/win32/llio/llio_common.o ../../src/win32/llio/llio_common.c
+
+${OBJECTDIR}/_ext/_DOTDOT/_DOTDOT/src/win32/launcher/launcher.o: nbproject/Makefile-${CND_CONF}.mk ../../src/win32/launcher/launcher.cpp 
+	${MKDIR} -p ${OBJECTDIR}/_ext/_DOTDOT/_DOTDOT/src/win32/launcher
+	${RM} $@.d
+	$(COMPILE.cc) -O2  -MMD -MP -MF $@.d -o ${OBJECTDIR}/_ext/_DOTDOT/_DOTDOT/src/win32/launcher/launcher.o ../../src/win32/launcher/launcher.cpp
+
+${OBJECTDIR}/_ext/_DOTDOT/_DOTDOT/src/win32/llio/org_catacombae_storage_io_win32_Win32FileStream.o: nbproject/Makefile-${CND_CONF}.mk ../../src/win32/llio/org_catacombae_storage_io_win32_Win32FileStream.c 
+	${MKDIR} -p ${OBJECTDIR}/_ext/_DOTDOT/_DOTDOT/src/win32/llio
+	${RM} $@.d
+	$(COMPILE.c) -O2  -MMD -MP -MF $@.d -o ${OBJECTDIR}/_ext/_DOTDOT/_DOTDOT/src/win32/llio/org_catacombae_storage_io_win32_Win32FileStream.o ../../src/win32/llio/org_catacombae_storage_io_win32_Win32FileStream.c
+
+${OBJECTDIR}/_ext/_DOTDOT/_DOTDOT/src/win32/llio/org_catacombae_storage_io_win32_ReadableWin32FileStream.o: nbproject/Makefile-${CND_CONF}.mk ../../src/win32/llio/org_catacombae_storage_io_win32_ReadableWin32FileStream.c 
+	${MKDIR} -p ${OBJECTDIR}/_ext/_DOTDOT/_DOTDOT/src/win32/llio
+	${RM} $@.d
+	$(COMPILE.c) -O2  -MMD -MP -MF $@.d -o ${OBJECTDIR}/_ext/_DOTDOT/_DOTDOT/src/win32/llio/org_catacombae_storage_io_win32_ReadableWin32FileStream.o ../../src/win32/llio/org_catacombae_storage_io_win32_ReadableWin32FileStream.c
+
+# Subprojects
+.build-subprojects:
+
+# Clean Targets
+.clean-conf: ${CLEAN_SUBPROJECTS}
+	${RM} -r build/Release
+	${RM} dist/Release/MinGW-Windows/libHFSExplorerNative.dll
+
+# Subprojects
+.clean-subprojects:
+
+# Enable dependency checking
+.dep.inc: .depcheck-impl
+
+include .dep.inc
---
+++ netbeans/HFSExplorerNative/nbproject/Makefile-variables.mk
@@ -0,0 +1,24 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+CND_BUILDDIR=build
+CND_DISTDIR=dist
+# Debug configuration
+CND_PLATFORM_Debug=MinGW-Windows
+CND_ARTIFACT_DIR_Debug=dist/Debug/MinGW-Windows
+CND_ARTIFACT_NAME_Debug=libHFSExplorerNative.dll
+CND_ARTIFACT_PATH_Debug=dist/Debug/MinGW-Windows/libHFSExplorerNative.dll
+CND_PACKAGE_DIR_Debug=dist/Debug/MinGW-Windows/package
+CND_PACKAGE_NAME_Debug=libHFSExplorerNative.dll.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/MinGW-Windows/package/libHFSExplorerNative.dll.tar
+# Release configuration
+CND_PLATFORM_Release=MinGW-Windows
+CND_ARTIFACT_DIR_Release=dist/Release/MinGW-Windows
+CND_ARTIFACT_NAME_Release=libHFSExplorerNative.dll
+CND_ARTIFACT_PATH_Release=dist/Release/MinGW-Windows/libHFSExplorerNative.dll
+CND_PACKAGE_DIR_Release=dist/Release/MinGW-Windows/package
+CND_PACKAGE_NAME_Release=libHFSExplorerNative.dll.tar
+CND_PACKAGE_PATH_Release=dist/Release/MinGW-Windows/package/libHFSExplorerNative.dll.tar
